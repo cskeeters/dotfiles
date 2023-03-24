3e60ed247b0798757fab91f13a7edc666f49e367
Chad Skeeters <chad.skeeters@gmail.com>
2023-03-24 14:53:31 -0500

    Adds configuration commands for macos

---
+++ config_macos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#### Finder
+
+# Finder: show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+#### Dock
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+#### Screenshots
+#
+# Save screenshots to the desktop
+defaults write com.apple.screencapture location -string "$HOME/Desktop"
+
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
