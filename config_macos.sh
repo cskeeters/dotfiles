6deacc902ba5d9b45f204a22ad6ed45b0d2d4461
Chad Skeeters <chad.skeeters@gmail.com>
2023-06-17 12:04:19 -0500

    Updates text file handling defaults in config_macos.sh

--- config_macos.sh
+++ config_macos.sh
@@ -20,3 +20,14 @@ defaults write com.apple.screencapture location -string "$HOME/Desktop"
 
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
+
+#### TextEdit
+
+# Open a blank document on start
+defaults write -g NSShowAppCentricOpenPanelInsteadOfUntitledFile -bool false
+
+# Do not autosave (may not work in ventura)
+defaults write com.apple.TextEdit ApplePersistence -bool no
+
+# Set default text editor to Sublime Text 4
+defaults write com.apple.LaunchServices/com.apple.launchservices.secure LSHandlers -array-add '{LSHandlerContentType=public.plain-text;LSHandlerRoleAll=com.sublimetext.4;}'
