df13efd9b1246a9c318159d1cad3dfaefa8e571b
Chad Skeeters <chad.skeeters@gmail.com>
2023-11-11 12:19:44 -0600

    Removes non-required vhs treesitter config

--- nvim/init.lua
+++ nvim/init.lua
@@ -361,17 +361,3 @@ require("lazy").setup("plugins")
 
 vim.keymap.set('n', '<Leader><Leader>l', ':Lazy<cr>', { desc="Opens the Lazy Package Manager UI" })
 
--- VHS
--- local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
--- parser_config.vhs = {
---   install_info = {
---     url = config_path.."/tree-sitter-vhs", -- local path or git repo
---     files = {"src/parser.c"}, -- note that some parsers also require src/scanner.c or src/scanner.cc
---     -- optional entries:
---     branch = "main", -- default branch in case of git repo if different from master
---     generate_requires_npm = false, -- if stand-alone parser without npm dependencies
---     requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
---   },
---   filetype = "vhs", -- if filetype does not match the parser name
--- }
--- vim.treesitter.language.register('python', 'vhs')  -- the vhs filetype will use the python parser and queries.
