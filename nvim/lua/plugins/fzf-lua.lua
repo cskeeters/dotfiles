fb5fcda6031eea8a6821b6e0dc0bba8df8477811
Chad Skeeters <chad.skeeters@gmail.com>
2023-11-11 12:18:53 -0600

    Improves description for searching for snippets

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -217,7 +217,7 @@ return {
     vim.keymap.set('n', '<Leader>pr', ':FzfLua register<cr>',  { desc="Paste register (fzf)" })
     vim.keymap.set('n', '<Leader><Leader>ft', ':FzfLua filetypes<cr>',  { desc="Select filetype to by applied to the current buffer setf (fzf)" })
     vim.keymap.set('n', '<LocalLeader>.', ':FzfLua spell_suggest<cr>',  { desc="Suggest spelling (fzf)" })
-    vim.keymap.set('n', '<LocalLeader>s', search_snippets,  { desc="Expand Snippet (fzf)" })
+    vim.keymap.set('n', '<LocalLeader>s', search_snippets,  { desc="Search for Snippet to insert (fzf)" })
     vim.keymap.set('n', '<LocalLeader><LocalLeader>s', edit_snippets,  { desc="Edit Snippet (fzf)" })
     vim.keymap.set('i', '<C-y>', search_snippets,  { desc="Expand Snippet (fzf)" })
 
