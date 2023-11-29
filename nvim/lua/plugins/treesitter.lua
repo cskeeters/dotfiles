ed19bdbe67ef8d89bef187bc1d6e7661df91b917
Chad Skeeters <chad.skeeters@gmail.com>
2023-11-29 12:53:05 -0600

    Minor changes to treesitter plus incremental selection

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,16 @@
 return {
     enabled = true,
     'nvim-treesitter/nvim-treesitter',
+    build = ":TSUpdate",
     keys = {
     },
     init = function()
-        require('nvim-treesitter.install').update { with_sync = true }
+        require'nvim-treesitter.configs'.setup {
+            modules = {},
 
-        require('nvim-treesitter.configs').setup {
+            -- Install parsers synchronously (only applied to `ensure_installed`)
+            sync_install = false,
+
             -- A list of parser names, or "all" (the four listed parsers should always be installed)
             ensure_installed = {
                 "c",
@@ -29,16 +33,24 @@ return {
                 "vim",
             },
 
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-
             -- Automatically install missing parsers when entering buffer
             -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
             auto_install = true,
 
+            ignore_install = {},
+
             highlight = {
                 enable = true,
+            },
+
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "gss",
+                    node_incremental = "gsn",
+                    scope_incremental = "gsc",
+                    node_decremental = "gsm",
+                }
             },
         }
     end,
