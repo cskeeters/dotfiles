36ca5030de47e137655029cf19932f8732ffc9dc
Chad Skeeters <chad.skeeters@gmail.com>
2023-12-13 14:11:14 -0600

    Disables tagbar

--- nvim/lua/plugins/tagbar.lua
+++ nvim/lua/plugins/tagbar.lua
@@ -1,9 +1,10 @@
 return {
-    enabled = true,
+    enabled = false,
     'majutsushi/tagbar',
     lazy=false,
     init = function()
         vim.g.tagbar_autofocus=1
+        vim.g.tagbar_autoclose=1
     end,
     keys = {
         {'<Leader>t', ':TagbarToggle<cr>', noremap=true, silent=true, desc="Toggles the Tagbar" },
