c3faf8fa33682712a4f98ea3bc460ae160cb623d
Chad Skeeters <chad.skeeters@gmail.com>
2024-01-02 18:29:23 -0600

    Updates neovim for linux to work with manually compiled neovim in /opt

--- shell/bashrc.d-Linux/neovim.sh
+++ shell/bashrc.d-Linux/neovim.sh
@@ -1,3 +1,9 @@
 if [[ -d /opt/neovim ]]; then
-    export PATH=/opt/neovim/bin:$PATH
+    export NEOVIM_HOME=/opt/neovim
+    export PATH=$NEOVIM_HOME/bin:$PATH
+
+    if [[ ! "$MANPATH" == *$NEOVIM_HOME/share/man* && -d "$NEOVIM_HOME/share/man" ]]; then
+        export MANPATH="$MANPATH:$NEOVIM_HOME/share/man"
+    fi
 fi
+
