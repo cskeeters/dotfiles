a6fe98f77d9be3f5eca0887d1c6e4bfe0071e1ca
Chad Skeeters <chad.skeeters@gmail.com>
2024-02-07 14:40:21 -0600

    Changes shortcut key for reload snippets

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -88,6 +88,6 @@ return {
         -- Install nvim-telescope/telescope-ui-select.nvim to use telescope
         vim.keymap.set("i", "<C-u>", require "luasnip.extras.select_choice", { desc='Select from choices'})
 
-        vim.keymap.set("n", "<leader><leader>s", reloadSnippets, { desc='Reload Snippets'})
+        vim.keymap.set("n", "<LocalLeader>rs", reloadSnippets, { desc='Reload Snippets'})
     end,
 }
