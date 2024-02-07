5686b1f38377068cab60e18457e3e08606295862
Chad Skeeters <chad.skeeters@gmail.com>
2024-02-07 14:39:53 -0600

    Turns off lazy change detection

--- nvim/init.lua
+++ nvim/init.lua
@@ -380,7 +380,15 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup({
+    spec = {
+        import = "plugins",
+    },
+    change_detection = {
+        enabled = false,
+        notify = false
+    },
+})
 
 vim.keymap.set('n', '<Leader><Leader>l', ':Lazy<cr>', { desc="Opens the Lazy Package Manager UI" })
 
