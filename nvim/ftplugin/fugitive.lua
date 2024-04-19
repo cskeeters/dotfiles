4ca94ce9d31aeeb5789acec590f9290470ba1e24
Chad Skeeters <chad.skeeters@gmail.com>
2024-04-19 10:27:13 -0500

    Adds key bindings for fugitive status and log

---
+++ nvim/ftplugin/fugitive.lua
@@ -0,0 +1,27 @@
+function Normal(keys)
+    local from_part = true; -- always true
+    local translate_less_than = true; --<lt>
+    local special = true; -- translate <CR> <Esc>
+    local translated_keys = vim.api.nvim_replace_termcodes(keys, from_part, translate_less_than, special);
+    vim.cmd.normal(translated_keys);
+end
+
+function FugitivePush()
+    vim.cmd([[G push]])
+end
+function FugitiveOpen()
+    Normal([[o<C-w><C-p><C-w>c]]);
+end
+function FugitiveDiff()
+    FugitiveOpen();
+    Normal([[<Leader>fd]]);
+end
+function FugitiveDiffTab()
+    Normal([[O<Leader>fD]]);
+end
+
+vim.keymap.set('n', '<leader>p', FugitivePush, { buffer=true, desc='Fugitive/git push' })
+vim.keymap.set('n', 'go', FugitiveOpen, { buffer=true, desc='Open file in current window' })
+vim.keymap.set('n', 'gd', FugitiveDiffTab, { buffer=true, desc='vimdiff on current file object' })
+
+vim.keymap.set('n', '<leader>fd', "O<Leader>fd", { buffer=true, desc="Fugitive/git diff" });
--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -1,7 +1,32 @@
+function FugitiveStatus()
+    vim.cmd([[0G]]);
+end
+
+function FugitiveLog()
+    vim.cmd([[0G log --oneline]]);
+end
+
+function FugitiveBlame()
+    vim.cmd([[G blame]]);
+    vim.cmd([[wincmd p]]);
+end
+
 return {
     enabled = true,
     'tpope/vim-fugitive',
     lazy = false,
-    keys = {
-    },
+    init = function()
+        vim.keymap.set('n', '<leader>fs', FugitiveStatus, { desc="Fugitive/git status" });
+        vim.keymap.set('n', '<leader>fl', FugitiveLog, { desc="Fugitive/git log" });
+        vim.keymap.set('n', '<leader>fb', FugitiveBlame, { desc="Fugitive/git blame" });
+
+        vim.keymap.set('n', '<leader>fd', ":Gdiffsplit<cr><C-w>l", { desc="vimdiff unstaged changes on current file" });
+        vim.keymap.set('n', '<leader>fD', ":Gdiffsplit @<cr><C-w>l", { desc="vimdiff staged changes on current file" });
+
+        vim.keymap.set('n', '<leader>fqd', ":G difftool<cr>", { desc="Load unstaged changes into quickfix list" });
+        vim.keymap.set('n', '<leader>fqD', ":G difftool --staged<cr>", { desc="Load staged changes into quickfix list" });
+
+        vim.keymap.set('n', '<leader>ftd', ":G difftool -y <cr>", { desc="vimdiff files with unstaged changes into tabs" });
+        -- vim.keymap.set('n', '<leader>ftD', ":G difftool -y --staged<cr>", { desc="vimdiff files with staged changes into tabs" }); this doesn't work
+    end,
 }
