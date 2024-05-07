302dac6078c41612a81f81d52ac2a74bef203bcd
Chad Skeeters <chad.skeeters@gmail.com>
2024-05-06 19:12:41 -0500

    Corrects spacing of comment

--- nvim/lua/plugins/kanagawa.lua
+++ nvim/lua/plugins/kanagawa.lua
@@ -30,7 +30,7 @@ return {
                 }
             end,
             theme = "dragon",              -- Load "wave" theme when 'background' option is not set
-            background = {               -- map the value of 'background' option to a theme
+            background = {                 -- map the value of 'background' option to a theme
                 dark = "dragon",           -- try "dragon" !
                 light = "lotus"
             },
