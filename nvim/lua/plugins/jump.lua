7669f4b00834aab5910044e1c03884997cf23dd9
Chad Skeeters <chad.skeeters@gmail.com>
2024-05-07 16:36:54 -0500

    Enables jump

--- nvim/lua/plugins/jump.lua
+++ nvim/lua/plugins/jump.lua
@@ -1,13 +1,14 @@
 return {
-    enabled = false,
+    enabled = true,
+    lazy = false,
     'cskeeters/vim-jump',
     keys = {
-        {'<Leader>gh', '<Plug>Jump', silent=true, desc="Jump (according to current line," },
+        {'<Leader>gj', '<Plug>Jump', silent=true, desc="Jump (according to current line," },
 
-        {'<Leader>mz', ':call GetMark("z",<cr>', noremap=true, desc="Save jump line to z register" },
-        {'<Leader>mx', ':call GetMark("x",<cr>', noremap=true, desc="Save jump line to x register" },
-        {'<Leader>mc', ':call GetMark("c",<cr>', noremap=true, desc="Save jump line to c register" },
-        {'<Leader>mv', ':call GetMark("v",<cr>', noremap=true, desc="Save jump line to v register" },
+        {'<Leader>mz', ':call GetMark("z")<cr>', noremap=true, desc="Save jump line to z register" },
+        {'<Leader>mx', ':call GetMark("x")<cr>', noremap=true, desc="Save jump line to x register" },
+        {'<Leader>mc', ':call GetMark("c")<cr>', noremap=true, desc="Save jump line to c register" },
+        {'<Leader>mv', ':call GetMark("v")<cr>', noremap=true, desc="Save jump line to v register" },
 
         {'<LocalLeader>z', ':put z<cr>', noremap=true, desc="Paste jump line from z register" },
         {'<LocalLeader>x', ':put x<cr>', noremap=true, desc="Paste jump line from x register" },
