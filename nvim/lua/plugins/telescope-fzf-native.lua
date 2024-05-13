899786b30b07fdf6fb287cfcce825aa0302b3a63
Chad Skeeters <chad.skeeters@gmail.com>
2024-05-12 22:25:47 -0500

    nvim: Disables fzf-native for telescope

--- nvim/lua/plugins/telescope-fzf-native.lua
+++ nvim/lua/plugins/telescope-fzf-native.lua
@@ -1,7 +1,7 @@
 -- Makes telescope accept fzf syntax by using cpp version of fzf (which is written in go btw)
 -- require('telescope').load_extension('fzf') in telescope.lua
 return {
-    enabled = true,
+    enabled = false,
     'nvim-telescope/telescope-fzf-native.nvim',
     -- build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
     build = 'make',
