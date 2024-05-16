0e38efbe56529901a461b8afaebfc338468de4c6
Chad Skeeters <chad.skeeters@gmail.com>
2024-05-16 09:51:34 -0500

    Adds keybindings for vim help files

---
+++ nvim/ftplugin/help.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', '<LocalLeader>`', [["_yiWi`<Esc>ea`<Esc>]], { buffer=true, desc='Monospace word under cursor' })
+vim.keymap.set('v', '<LocalLeader>`', [[s`<C-r>"`<Esc>]], { buffer=true, desc='Monospace selected text' })
