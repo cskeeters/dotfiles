42acfaba8bf5a3db31d3a696c9eb0f63b9a20127
Chad Skeeters <chad.skeeters@gmail.com>
2024-06-04 12:01:31 -0500

    nvim: Configures javadoc for bcst

--- nvim/lua/plugins/javadoc.lua
+++ nvim/lua/plugins/javadoc.lua
@@ -3,7 +3,8 @@ return {
     'cskeeters/javadoc.nvim',
     ft="java", -- Lazy load this plugin as soon as we open a file with java filetype
     init = function()
-        vim.g.javadoc_path="/Users/chad/local_reference/java8_doc/api"
+        vim.g.javadoc_path="/opt/AMS2/net/doc/java8/api:/usr/local/Solipsys/TDF4.7.13/doc/api:/opt/AMS2/net/doc/apache-log4j-1.2.17/apidocs"
+        vim.g.javadoc_debug=0
     end,
     keys = {
         {'K', '<Plug>JavadocOpen', buffer=true, ft="java", desc='Open javadoc api to work under cursor'},
