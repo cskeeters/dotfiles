d72a1338610912f66fd6ad14418ec2cc337719f2
Chad Skeeters <chad.skeeters@gmail.com>
2024-08-30 15:11:26 -0500

    Adds go build keymap

--- nvim/ftplugin/go.lua
+++ nvim/ftplugin/go.lua
@@ -1,2 +1,4 @@
 vim.opt_local.expandtab = false             -- Use tabs
 vim.opt_local.listchars = "trail:·,precedes:«,extends:»,tab:  ,nbsp:·,lead:·"
+
+vim.keymap.set('n', '<C-k>', [[<Cmd>!go build<cr>]], { buffer=true, desc='Build/Compile' })
