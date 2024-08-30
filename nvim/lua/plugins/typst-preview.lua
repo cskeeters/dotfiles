8921b509b12dabdc3acdffb79f6d87b0af90a4ab
Chad Skeeters <chad.skeeters@gmail.com>
2024-08-30 15:11:03 -0500

    Updates typst-preview plugin

--- nvim/lua/plugins/typst-preview.lua
+++ nvim/lua/plugins/typst-preview.lua
@@ -1,8 +1,9 @@
+-- Be sure to run TypstPreviewUpdate if there are issues
 return {
+    enabled = true,
     'chomosuke/typst-preview.nvim',
     lazy = true,
     ft = {'typst'},
-    version = '0.1.*',
     build = function() require 'typst-preview'.update() end,
     config = function()
         vim.keymap.set('n', '<C-k>v', '<Cmd>TypstPreviewToggle<Cr>', { buffer=true, silent=true, desc="Toggle Typst Preview" })
