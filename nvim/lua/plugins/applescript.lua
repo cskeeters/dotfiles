70aca8172aadbce9465bfabe76651c98a5391fab
Chad Skeeters <chad.skeeters@gmail.com>
2024-08-30 15:24:17 -0500

    Adds applescript plugin

---
+++ nvim/lua/plugins/applescript.lua
@@ -0,0 +1,8 @@
+return {
+    enabled = false,
+    'mityu/vim-applescript',
+    lazy = false,
+    keys = {
+        { '<LocalLeader>r', '<cmd>AppleScriptRun<cr>', noremap = true, desc="Run Applescript" },
+    },
+}
