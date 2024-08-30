f96dcd69fd0b2d8d44b8fe753c738d182a8db7ca
Chad Skeeters <chad.skeeters@gmail.com>
2024-08-30 15:09:38 -0500

    Removes binutils from path as it conflicts with release builds of rust/cargo

--- shell/bashrc.d-arya/binutils.sh
+++ shell/bashrc.d-arya/binutils.sh
@@ -1,3 +1,4 @@
-export PATH="/opt/homebrew/opt/binutils/bin:$PATH"
-export LDFLAGS="-L/opt/homebrew/opt/binutils/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/binutils/include"
+# cargo build --release doesn't work with strip from binutils
+#export PATH="/opt/homebrew/opt/binutils/bin:$PATH"
+#export LDFLAGS="-L/opt/homebrew/opt/binutils/lib"
+#export CPPFLAGS="-I/opt/homebrew/opt/binutils/include"
