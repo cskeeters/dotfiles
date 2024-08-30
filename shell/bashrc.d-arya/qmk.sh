44f0f9608dca007aafd257a513ae18a788c64ef3
Chad Skeeters <chad.skeeters@gmail.com>
2024-08-30 14:10:23 -0500

    Updates PATHs for arm tools for qmk

--- shell/bashrc.d-arya/qmk.sh
+++ shell/bashrc.d-arya/qmk.sh
@@ -1,3 +1,5 @@
 #### QMK
 export PATH="$BREW_PREFIX/opt/avr-gcc@8/bin:$PATH"
-export PATH="$BREW_PREFIX/opt/arm-gcc-bin@8/bin:$PATH"
+# export PATH="$BREW_PREFIX/opt/arm-gcc-bin@8/bin:$PATH"
+export PATH="$BREW_PREFIX/opt/arm-none-eabi-binutils/bin:$PATH"
+export PATH="$BREW_PREFIX/opt/arm-none-eabi-gcc@8/bin:$PATH"
