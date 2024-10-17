e303ba5b7fbb20859be57a4fd63aa91dd23ded08
Chad Skeeters <chad.skeeters@gmail.com>
2024-10-17 14:54:30 -0500

    Adds cmdsnip to bash config

---
+++ shell/cmdsnip.sh
@@ -0,0 +1,84 @@
+#### Command Line Snippets
+
+getsnippet() {
+    TRIGGER="$1"
+    while IFS= read -r -d $'\0' file; do
+        if egrep "^snippet $TRIGGER" "$file" > /dev/null; then
+            gsed -nre "/snippet $TRIGGER/{:again; n; s/\t(.*)/\1/p;t again;q;}" "$file"
+        fi
+    done < <(find ~/.config/cmd -type f -print0)
+}
+
+export -f getsnippet
+
+snippreview() {
+    TRIGGER=""
+
+    re='^snippet +([^[:space:]]*)'
+    if [[ "$1" =~ $re ]]; then
+        TRIGGER=${BASH_REMATCH[1]}
+    fi
+
+    if [[ -z $TRIGGER ]]; then
+        return
+    fi
+
+    SNIPPET=$(getsnippet "$TRIGGER")
+    echo $SNIPPET
+}
+export -f snippreview
+
+runsnippet() {
+    local DEFAULT
+
+    S=$(cat $HOME/.config/cmd/* | grep snippet | \
+        fzf -d ' ' --with-nth 3.. --bind 'enter:become(echo {2})' \
+            --preview-window='top,10%' \
+            --preview 'snippreview {}'
+    )
+    if [[ $S != "" ]]; then
+        #printf 'Processing Snippet: %q\n' "$S"
+        SNIPPET=$(getsnippet "$S")
+
+        #echo "$SNIPPET"
+        CMD="$SNIPPET"
+
+        for ((i=1;i<10;i++)); do
+            if echo $SNIPPET | egrep '\$\{'"$i"'.*\}' > /dev/null; then
+                DEFAULT=$(echo $SNIPPET | sed -nre 's/.*\$\{'"$i"':([^\}]*)\}.*/\1/p')
+                echo "Populating: $CMD"
+                if [[ $DEFAULT =~ ^(.*)\(\)$ ]]; then
+                    INPUTFUNC=${BASH_REMATCH[1]}
+                    VALUE=$($INPUTFUNC)
+                else
+                    VALUE=$(gum input --prompt='$1 >' --value="$DEFAULT" --placeholder="$DEFAULT")
+                fi
+                #echo "$i -> $DEFAULT"
+
+                CMD=$(echo "$CMD" | sed -re 's/\$\{'"$i"'.*\}/'"$VALUE"'/g')
+            fi
+        done
+
+        # Populate the promopt
+        READLINE_LINE="$CMD"
+        # Cursor position at the end
+        READLINE_POINT=${#READLINE_LINE}
+    fi
+}
+
+bind -x '"\C-x\C-j": runsnippet'
+
+###### Functions for use in snippets
+
+git_select_modified() {
+    git ls-files -m | fzf --height="90%" --preview 'git diff --color=always {}' --preview-window='top,50%'
+}
+
+git_select_staged() {
+    git diff --name-only --staged | fzf --height="90%"--preview 'git diff --staged --color=always {}' --preview-window='top,50%'
+}
+
+git_tags() {
+    git tag -l | fzf --height="90%" --preview 'git log --color=always --graph --oneline -3 {}' --preview-window='top,10%'
+}
+
