5ceef407b7b1604aef695c5266868219fcc4920d
Chad Skeeters <chad.skeeters@gmail.com>
2024-10-17 17:25:55 -0500

    Adds img-clip plugin to neovim

---
+++ nvim/lua/plugins/img-clip.lua
@@ -0,0 +1,8 @@
+return {
+    enabled = true,
+    lazy = false,
+    "HakonHarnes/img-clip.nvim",
+    keys = {
+        { ",,p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+    },
+}
