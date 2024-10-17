75e9a2d630a4dfd117cf345b3a503a18be1248a5
Chad Skeeters <chad.skeeters@gmail.com>
2024-10-17 13:17:28 -0500

    Adds documentation on pip modules required for vim-beancount

--- nvim/lua/plugins/beancount.lua
+++ nvim/lua/plugins/beancount.lua
@@ -1,3 +1,10 @@
+-- python3 -m pip config set global.break-system-packages true
+-- python3 -m pip install pynvim --upgrade
+-- python3 -m pip install beancount --upgrade
+-- or --
+-- python3 -m pip install pynvim --break-system-packages
+-- python3 -m pip install beancount --break-system-packages
+
 return {
     enabled = true,
     'nathangrigg/vim-beancount',
