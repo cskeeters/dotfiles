5db3a8437da1bb55bf174c8e4e40c02ec1774d54
Chad Skeeters <chad.skeeters@gmail.com>
2024-10-17 13:23:07 -0500

    Adds groff configuration

---
+++ nvim/ftdetect/groff.vim
@@ -0,0 +1,4 @@
+autocmd BufRead,BufNewFile *.me  set filetype=groff.me
+autocmd BufRead,BufNewFile *.mm  set filetype=groff.mm
+autocmd BufRead,BufNewFile *.ms  set filetype=groff.ms
+autocmd BufRead,BufNewFile *.mom set filetype=groff.mom
---
+++ nvim/ftplugin/mom.lua
@@ -0,0 +1,18 @@
+vim.opt_local.wrap = true
+
+vim.keymap.set('n', '<LocalLeader>j', [[O.NH 1<Esc>j0]], { buffer=true, desc='Heading 1' })
+vim.keymap.set('n', '<LocalLeader>k', [[O.NH 2<Esc>j0]], { buffer=true, desc='Heading 2' })
+
+-- Text styling Bold Italic Fixed-width
+-- Note: "_yiw moves cursor to the beginning of the word affecting any registers.  B doen't work when cursor is already on the first character
+-- Note: Changed E to e because punctuation is more of a problem than hyphens in words needing style
+vim.keymap.set('n', '<LocalLeader>b', [["_yiWi\*[BD]<Esc>ea\*[PREV]<Esc>]], { buffer=true, desc='Bold word under cursor' })
+vim.keymap.set('n', '<LocalLeader>i', [["_yiWi\*[IT]<Esc>ea\*[PREV]<Esc>]], { buffer=true, desc='Italicize word under cursor' })
+vim.keymap.set('n', '<LocalLeader>`', [["_yiWi\f[CR]<Esc>ea\f[]<Esc>]], { buffer=true, desc='Monospace word under cursor' })
+
+vim.keymap.set('v', '<LocalLeader>b', [[s\*[BD]<C-r>"\*[PREV]<Esc>]], { buffer=true, desc='Bold selected text' })
+vim.keymap.set('v', '<LocalLeader>i', [[s\*[IT]<C-r>"\*[PREV]<Esc>]], { buffer=true, desc='Italicize selected text' })
+vim.keymap.set('v', '<LocalLeader>`', [[s\f[CB]<C-r>"\f[]<Esc>]], { buffer=true, desc='Monospace selected text' })
+
+-- build
+vim.keymap.set('n', '<C-k>', [[<Cmd>update<cr><Cmd>silent !pdfmom -Kutf8 -t % > %:r.pdf && open -a Preview.app %:r.pdf<cr>]], { buffer=true, desc='Build/Compile to PDF' })
