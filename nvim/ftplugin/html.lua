3831752ec8364617560e8b3d250f5b6ee89bcbbc
Chad Skeeters <chad.skeeters@gmail.com>
2024-10-17 13:20:51 -0500

    Adds to html configuration in neovim

--- nvim/ftplugin/html.lua
+++ nvim/ftplugin/html.lua
@@ -4,4 +4,6 @@ vim.keymap.set('n', '<Leader>b', 'diwi<strong><escape>pa</strong><escape>', { bu
 vim.keymap.set('n', '<Leader>i', 'diwi<em><escape>pa</em><escape>', { buffer=true, desc='Italicize word'})
 vim.keymap.set('n', '<Leader>`', 'diwi<code><escape>pa</code><escape>', { buffer=true, desc='Monospace word'})
 
-vim.keymap.set('n', '<C-K>v', '<cmd>!open "%"<cr>', { buffer=true, desc='View HTML in browser' })
+vim.keymap.set('n', '<C-K>o', '<cmd>!open "%"<cr>', { buffer=true, desc='View HTML in browser' })
+
+vim.keymap.set('n', '<C-k>v', '<Cmd>BrowserPreview<Cr>', { buffer=true, silent=true, desc="Open Browsersync Preview" })
