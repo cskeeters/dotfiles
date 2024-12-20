b3d050ebe8da14d8d9e40e80dfd4b1a567bef7e0
Chad Skeeters <chad.skeeters@gmail.com>
2024-12-19 23:07:04 -0600

    Updates CleanClip.spoon to use copy of clipboard

--- hammerspoon/Spoons/CleanClip.spoon/init.lua
+++ hammerspoon/Spoons/CleanClip.spoon/init.lua
@@ -1,8 +1,23 @@
 -- This spoon enables the removal of formatting from text in the clipboard.
--- This watches the clipboard and when new data is copied to the clipboard it
--- will mutate the text based on the current mode.  The modes are as follows:
---  * 
 --
+-- This watches the clipboard for changes.  If the clipboard changes because
+-- this script modified it, the UTI saved in old_uti will have been set and
+-- the change can be ignored.
+--
+-- When the data as no value for old_uti, it's fresh.  The data is saved into
+-- current_all_data, is mutated according to mode, and written back to the
+-- clipboard.
+--
+-- When the mode is changed, the data is remodified from the copy in 
+-- current_all_data.  This prevents the user from having to re-copy the data if
+-- it has been mutated undesirably.
+--
+-- The modes are:
+--   OFF   - no modification takes place
+--   TEXT  - formatting is stripped from text  (only the text_uti is included
+--           in the mutated clipboard)
+--   EXCEL - Text is trimmed, '$' and ','s are removed, good for accounting
+--           cells to other cells where formatting should be retained.
 
 local obj = {
 }
@@ -15,6 +30,9 @@ obj.homepage = "https://github.com/cskeeters/hammerspoon-CleanClip"
 obj.license = "https://github.com/cskeeters/hammerspoon-CleanClip"
 
 local watcher = nil
+local current_all_data = nil
+local text_uti = "public.utf8-plain-text"   -- UTI to mutate when mode is not OFF
+local old_uti = "vnd.sqlite3"               -- Should be a valid UTI that is not commonly used
 
 obj.OFF = 0
 obj.TEXT_ONLY = 1
@@ -27,12 +45,15 @@ function obj:setMode(m)
 
     if obj.mode == obj.OFF then
         hs.alert.show("CleanClip Mode: Off")
+        SetRaw()
     end
     if obj.mode == obj.TEXT_ONLY then
         hs.alert.show("CleanClip Mode: Text Only")
+        SetText()
     end
     if obj.mode == obj.TEXT_EXCEL then
         hs.alert.show("CleanClip Mode: Text / Excel")
+        SetExcel()
     end
 end
 
@@ -87,21 +108,86 @@ local function cleanExcel(data)
     return table.concat(mlines, "\n")
 end
 
+-- Returns true when new data is detected and copied
+function save_raw()
+    -- In case we need to see what UTIs are being set
+    -- local types = hs.pasteboard.contentTypes()
+    -- print("types")
+    -- for _,v in ipairs(types) do
+        -- print(v)
+    -- end
+
+    local all_data = hs.pasteboard.readAllData()
+    if all_data ~= nil then
+        if all_data[old_uti] == nil then
+            current_all_data = all_data
+            -- print("Saving clipboard data")
+            return true
+        end
+    end
+
+    return false
+end
+
+function table.shallow_copy(t)
+  local copy = {}
+  for k,v in pairs(t) do
+    copy[k] = v
+  end
+  return copy
+end
+
+function SetRaw()
+    if current_all_data ~= nil then
+        local copy = table.shallow_copy(current_all_data)
+        copy[old_uti] = "1"
+        if hs.pasteboard.writeAllData(copy) ~= true then
+            hs.alert.show("Error setting clipboard")
+        end
+    end
+end
+
+function SetText()
+    if current_all_data ~= nil then
+        -- If the current data has no value for text_uti, leave the raw data in place
+        if current_all_data[text_uti] ~= nil then
+            local all_data = {}
+            -- skip copying all other UTIs
+            all_data[text_uti] = current_all_data[text_uti]
+            all_data[old_uti] = "1"
+            if hs.pasteboard.writeAllData(all_data) ~= true then
+                hs.alert.show("Error setting clipboard")
+            end
+        end
+    end
+end
+
+function SetExcel()
+    if current_all_data ~= nil then
+        -- If the current data has no value for text_uti, leave the raw data in place
+        if current_all_data[text_uti] ~= nil then
+            local all_data = {}
+            -- skip copying all other UTIs
+            all_data[text_uti] = cleanExcel(current_all_data[text_uti])
+            all_data[old_uti] = "1"
+            if hs.pasteboard.writeAllData(all_data) ~= true then
+                hs.alert.show("Error setting clipboard")
+            end
+        end
+    end
+end
+
 function obj:clipboardCallback()
-    if obj.mode == obj.OFF then
-        return
+    -- print("clipboardCallback")
+    if save_raw() then
+        -- we captured a change
+        if obj.mode == obj.TEXT_ONLY then
+            SetText()
+        end
+        if obj.mode == obj.TEXT_EXCEL then
+            SetExcel()
+        end
     end
-
-    local clipboard = hs.pasteboard.getContents()
-
-    if obj.mode == obj.TEXT_EXCEL then
-        clipboard = cleanExcel(clipboard)
-    end
-
-    if hs.pasteboard.setContents(clipboard) ~= true then
-        hs.alert.show("Error setting clipboard")
-    end
-    -- print("Modified clipboard to: "..clipboard)
 end
 
 -- Binds global hotkeys
@@ -113,6 +199,7 @@ function obj:init()
     -- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
 
     watcher = hs.pasteboard.watcher.new(function() obj:clipboardCallback() end)
+    hs.pasteboard.watcher.interval(1.0/8)
     watcher:start()
 end
 
