09b68b5ca6ceecbd728ae368ed650390e78ecd86
Chad Skeeters <chad.skeeters@gmail.com>
2025-02-07 12:57:37 -0600

    Fixes keymappings for vim-table-mode

--- nvim/lua/plugins/table-mode.lua
+++ nvim/lua/plugins/table-mode.lua
@@ -3,6 +3,23 @@ return {
     'dhruvasagar/vim-table-mode',
     lazy = false,
     init = function()
-        vim.keymap.set('n', ',,tm', '<Cmd>TableModeToggle<Cr>', { buffer=true, silent=true, desc="Toggle Table Mode" })
+        vim.g.table_mode_map_prefix = ',,t'
+        vim.g.table_mode_disable_mappings = 0
+        vim.g.table_mode_disable_tableize_mappings = 1
+    end,
+    config = function()
+        vim.keymap.set('n', '\\\\tm', '<Cmd>TableModeToggle<Cr>', { desc="Toggle Table Mode" })
     end
 }
+
+--[[
+Usage:
+
+1. Create the header.
+2. "||" on the next line will make an hline.
+3. Put colons to align columns.
+
+|foo|bar|
+||
+
+--]]
