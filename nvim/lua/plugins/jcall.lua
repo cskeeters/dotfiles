6cc8dcbb4b7368941b273e5412e8a2a47cb0babb
Chad Skeeters <chad.skeeters@gmail.com>
2025-02-07 12:59:02 -0600

    Fixes bug in jcall plugin config

--- nvim/lua/plugins/jcall.lua
+++ nvim/lua/plugins/jcall.lua
@@ -1,6 +1,6 @@
 return {
     enabled = false,
-    ft = java,
+    ft = "java",
     'cskeeters/jcall.vim',
     keys = {
         {'<Leader>ch',  '<Plug>JCallOpen',  buffer=true, ft='java', desc='Populates the quickfix window with calls to the method under the cursor'},
