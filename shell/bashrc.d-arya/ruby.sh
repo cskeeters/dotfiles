2f567a4f008180519fb6f2a8707cd1f19a885360
Chad Skeeters <chad.skeeters@gmail.com>
2025-04-09 00:10:12 -0500

    Updates ruby to better support PKG_CONFIG_PATH

--- shell/bashrc.d-arya/ruby.sh
+++ shell/bashrc.d-arya/ruby.sh
@@ -1,8 +1,8 @@
 #### Ruby
-#
+
 export PATH="$BREW_PREFIX/opt/ruby/bin:$PATH"
 export LDFLAGS="-L$BREW_PREFIX/opt/ruby/lib"
 export CPPFLAGS="-I$BREW_PREFIX/opt/ruby/include"
-export PKG_CONFIG_PATH="$BREW_PREFIX/opt/ruby/lib/pkgconfig"
-#
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$BREW_PREFIX/opt/ruby/lib/pkgconfig"
+
 #export PATH="$BREW_PREFIX/lib/ruby/gems/3.2.0/bin:$PATH"
