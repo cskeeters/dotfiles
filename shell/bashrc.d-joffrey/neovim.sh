cde4268274f58cb996d837f0eb44b8f9b114e3ba
Chad Skeeters <chad.skeeters@gmail.com>
2025-04-09 08:32:15 -0500

    Adds neovim config for joffrey

--- shell/bashrc.d-joffrey/neovim.sh
+++ shell/bashrc.d-joffrey/neovim.sh
@@ -1,6 +1,4 @@
-NEOVIM_HOME=/opt/neovim
-PATH=$NEOVIM_HOME/bin:$PATH
-
-if [[ ! "$MANPATH" == *$NEOVIM_HOME/share/man* && -d "$NEOVIM_HOME/share/man" ]]; then
-    export MANPATH="$MANPATH:$NEOVIM_HOME/share/man"
+if exists nvim; then
+    alias vi='nvim'
+    alias vim='nvim'
 fi
