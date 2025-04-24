cf1b34cca8aed0e8ca9c5918cde4d36b26617c77
Chad Skeeters <chad.skeeters@gmail.com>
2025-04-24 14:44:05 -0500

    Disables typst plugin as type tree-sitter parser overrides

--- nvim/lua/plugins/typst.lua
+++ nvim/lua/plugins/typst.lua
@@ -1,5 +1,12 @@
 return {
+    enabled = false, -- using tree-sitter parser
     'kaarmu/typst.vim',
     ft = 'typst',
-    lazy=false,
+    lazy = false,
+    init = function()
+        vim.g.typst_conceal = 0
+        vim.g.typst_conceal_math = 0
+        vim.g.typst_conceal_emoji = 0
+        vim.g.typst_embedded_languages = 0
+    end,
 }
