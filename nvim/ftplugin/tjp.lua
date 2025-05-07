aec83aeed5c6d6b558f1b6ee8a62e399d36b17be
Chad Skeeters <chad.skeeters@gmail.com>
2025-05-06 19:16:05 -0500

    Corrects boolean case in task juggler config

--- nvim/ftplugin/tjp.lua
+++ nvim/ftplugin/tjp.lua
@@ -1,4 +1,4 @@
 vim.bo.commentstring = '# %s'
-vim.bo.foldenable = False
+vim.bo.foldenable = false
 
 vim.keymap.set('n', '<C-k>', [[<cmd>update | lcs %:p:h | !mkdir -p output | !( cd output; tj3 --no-color '%:p' && open %:r.html )<cr>]], { buffer=true, desc='Generate HTML for Task Juggler' })
