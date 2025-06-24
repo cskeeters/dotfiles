3cee0647e30ec682030841d44bb0e9038f876491
Chad Skeeters <chad.skeeters@gmail.com>
2025-06-24 14:43:30 -0500

    Modifies asciidoc keybinding to open the resulting html

--- nvim/ftplugin/asciidoc.lua
+++ nvim/ftplugin/asciidoc.lua
@@ -1,1 +1,1 @@
-vim.keymap.set('n', '<C-K>v', '<cmd>!asciidoc -b html5 -a icons -a toc2 -a theme=flask "%"<cr>', { buffer=true, desc='Convert asciidoc to html' })
+vim.keymap.set('n', '<C-K>v', [[<cmd>!asciidoc -b html5 -a icons -a toc2 -a theme=flask "%" && open '%:r.html'<cr>]], { buffer=true, desc='Convert asciidoc to html' })
