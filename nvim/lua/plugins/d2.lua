44956e0acf29f4bdcbc6dc4e259f2dc8282f4cfa
Chad Skeeters <chad.skeeters@gmail.com>
2025-08-15 00:00:04 -0500

    Disables d2-vim as it interferes with Java syntax in runtime

--- nvim/lua/plugins/d2.lua
+++ nvim/lua/plugins/d2.lua
@@ -1,5 +1,7 @@
+-- Currently this causes an issue with syntax highlighting with java files.
+--   https://github.com/terrastruct/d2-vim/issues/26
 return {
-    enabled = true,
+    enabled = false,
     'terrastruct/d2-vim',
     lazy = false,
     ft = 'd2',
