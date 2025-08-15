31160c0a5045789ab66a8ba6ec47083319300916
Chad Skeeters <chad.skeeters@gmail.com>
2025-08-14 23:53:37 -0500

    Updates markdown-preview so it will compile when installing without error

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -1,10 +1,22 @@
 return {
+    enabled = true,
     "iamcco/markdown-preview.nvim",
     lazy = true,
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown", "quarto" },
-    build = function() vim.fn["mkdp#util#install"]() end,
-    config = function()
-        vim.g.mkdp_filetypes = { "markdown", "quarto" }
-    end
+    build = function(plugin)
+        if vim.fn.executable "npx" then
+            vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
+        else
+            vim.cmd [[Lazy load markdown-preview.nvim]]
+            vim.fn["mkdp#util#install"]()
+        end
+    end,
+    init = function()
+        if vim.fn.executable "npx" then vim.g.mkdp_filetypes = { "markdown" } end
+    end,
+    -- config = function()
+        -- vim.g.mkdp_filetypes = { "markdown", "quarto" }
+    -- end
 }
+
