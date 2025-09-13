1e695d19ae99e05bc41efedecfd2f926c0ea1b69
Chad Skeeters <chad.skeeters@gmail.com>
2025-09-13 14:10:13 -0500

    Updates keymappings for fidget (notifications)

--- nvim/lua/plugins/fidget.lua
+++ nvim/lua/plugins/fidget.lua
@@ -1,11 +1,21 @@
 return {
     enabled = true,
     "j-hui/fidget.nvim",
+    dependencies = {
+        'nvim-telescope/telescope.nvim',
+    },
     opts = {
         notification = {
             override_vim_notify = true,
         },
     },
--- require("telescope").extensions.fidget.fidget()
--- require("telescope").load_extension("fidget")
+    init = function()
+        vim.notify("fidget config")
+        -- require("telescope").extensions.fidget.fidget()
+        require("telescope").load_extension("fidget")
+
+        vim.keymap.set('n', '<Leader>gn', '<Cmd>Fidget clear<cr>',    { desc="Clear notifications" })
+        vim.keymap.set('n', '<Leader>N', '<Cmd>Fidget history<cr>',   { desc="List notifications" })
+        vim.keymap.set('n', '<Leader>n', '<Cmd>Telescope fidget<cr>', { desc="List notifications with telescope" })
+    end,
 }
