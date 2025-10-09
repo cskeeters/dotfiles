2f41249f76e6f4157096ef81719e83a34c42f87d
Chad Skeeters <chad.skeeters@gmail.com>
2025-10-09 08:54:10 -0500

    Updates bfm plugins to support new plugin API

--- bfm/plugins/helper.sh
+++ bfm/plugins/helper.sh
@@ -5,12 +5,24 @@ die() {
 
 read_state() {
     read -r CUR_DIR
-    read -r HOVERED_FILE
+
+    PATHS=()
+    while IFS= read -r P; do
+        PATHS+=("$P")
+    done
 }
 
-STATE_FILE="$1"
+STATE_FILE=$1
 shift
-CMD_FILE="$1"
+CMD_FILE=$1
 shift
 
 read_state < "$STATE_FILE"
+HOVERED_PATH=${PATHS[0]}
+HOVERED_FILE=$(basename "${PATHS[0]}")
+
+# Pass all paths as arguments to `foo`:
+#   foo "${PATHS[@]}"
+#
+# Run command once for each argument:
+#   printf '%s\n' "${PATHS[@]}" | xargs -I {} cmd "{}"
