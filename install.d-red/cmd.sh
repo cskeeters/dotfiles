a65652a13e974063d29a270f7474abf803964505
Chad Skeeters <chad.skeeters@gmail.com>
2025-10-09 13:55:13 -0500

    Updates cmd snippets for machines

---
+++ install.d-red/cmd.sh
@@ -0,0 +1,7 @@
+link cmd/apt.snippets                       .config/cmd
+link cmd/git.snippets                       .config/cmd
+link cmd/gnu.snippets                       .config/cmd
+link cmd/luarocks.snippets                  .config/cmd
+link cmd/restic.snippets                    .config/cmd
+link cmd/tar.snippets                       .config/cmd
+link cmd/zip.snippets                       .config/cmd
