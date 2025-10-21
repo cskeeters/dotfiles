2eece4512b8839e083b3fea76cb2b6538046c516
Chad Skeeters <chad.skeeters@gmail.com>
2025-10-21 10:33:01 -0500

    Adds floatterm nvim plugin

---
+++ nvim/lua/plugins/floatterm.lua
@@ -0,0 +1,20 @@
+local function ToggleTermOn()
+    -- FloatermToggle doesn't take parameters
+    vim.cmd([[FloatermToggle POPUP]])
+    -- But we can set them with FloatermUpdate
+    vim.cmd([[FloatermUpdate --name=POPUP --height=0.8 --width=0.9 --wintype=vsplit --autoclose=1]])
+end
+
+return {
+    enabled = true,
+    'voldikss/vim-floaterm',
+
+    config = function()
+        -- autoclose:   0: do not close   1: do not close on error   2: always close
+        vim.keymap.set('n', '<Leader>gl', ':FloatermNew --height=0.9 --width=0.9 --wintype=float --name=LazyGit --position=center --autoclose=1 lazygit<cr>',
+                                                                       { desc="Open file in current directory tree (fzf)" })
+
+        vim.keymap.set('n', '<F1>', ToggleTermOn,                      { desc="Shows the floating terminal" })
+        vim.keymap.set('t', '<F1>', [[<C-\><C-n>:FloatermToggle<CR>]], { desc="Hides the floating terminal" })
+    end,
+}
