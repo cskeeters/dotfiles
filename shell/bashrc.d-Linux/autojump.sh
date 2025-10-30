4228cbad5094aa07d710d0c91eee175a231dbb15
Chad Skeeters <chad.skeeters@gmail.com>
2025-10-29 19:35:56 -0500

    flexible autojump init for linux

--- shell/bashrc.d-Linux/autojump.sh
+++ shell/bashrc.d-Linux/autojump.sh
@@ -1,2 +1,7 @@
-CONF=/usr/share/autojump/autojump.bash
-[[ -f "$CONF" ]] && source "$CONF"
+if [[ -s "$HOME/.autojump/etc/profile.d/autojump.bash" ]]; then
+    source "$HOME/.autojump/etc/profile.d/autojump.bash"
+elif [[ -s /usr/share/autojump/autojump.bash ]]; then
+    source /usr/share/autojump/autojump.bash
+elif [[ -s /usr/share/autojump/etc/profile.d/autojump.bash ]]; then
+    source /usr/share/autojump/etc/profile.d/autojump.bash
+fi
