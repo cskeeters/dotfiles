1ad9d3a2df3507160b03e0ea11319a8531f0f489
Chad Skeeters <chad.skeeters@gmail.com>
2025-10-31 09:27:46 -0500

    Modifies neovim config to alias vi and vim after PATH has been updated

--- shell/bashrc.d-Linux/neovim.sh
+++ shell/bashrc.d-Linux/neovim.sh
@@ -1,8 +1,3 @@
-if exists nvim; then
-    alias vi='nvim'
-    alias vim='nvim'
-fi
-
 if [[ -d /opt/neovim ]]; then
     export NEOVIM_HOME=/opt/neovim
     export PATH=$NEOVIM_HOME/bin:$PATH
@@ -10,5 +5,10 @@ if [[ -d /opt/neovim ]]; then
     if [[ ! "$MANPATH" == *$NEOVIM_HOME/share/man* && -d "$NEOVIM_HOME/share/man" ]]; then
         export MANPATH="$MANPATH:$NEOVIM_HOME/share/man"
     fi
+
 fi
 
+if exists nvim; then
+    alias vi='nvim'
+    alias vim='nvim'
+fi
