bc9ec359a9e8358beb49fe0f9e0dda9803aa02e5
Chad Skeeters <chad.skeeters@gmail.com>
2025-11-02 16:26:09 -0600

    lua/luarock configuration

--- nvim/lua/plugins/luarocks.lua
+++ nvim/lua/plugins/luarocks.lua
@@ -1,11 +1,12 @@
 return {
-    enabled = true,
+    enabled = false,
     "vhyrro/luarocks.nvim",
     priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
     config = true,
     opts = {
         rocks = {
             "luautf8",
+            "luafilesystem",
             -- "lrexlib-pcre" needed for kokoro.  See notes/neovim.md
         },
     }
