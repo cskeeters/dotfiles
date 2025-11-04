7d5f1cad8acec9ae60152c291b2ca91e7160dd41
Chad Skeeters <chad.skeeters@gmail.com>
2025-11-04 08:52:20 -0600

    Moves base snippets to install

--- install.d-red/cmd.sh
+++
@@ -1,8 +0,0 @@
-link cmd/autojump.snippets                       .config/cmd
-link cmd/apt.snippets                       .config/cmd
-link cmd/git.snippets                       .config/cmd
-link cmd/gnu.snippets                       .config/cmd
-link cmd/luarocks.snippets                  .config/cmd
-link cmd/restic.snippets                    .config/cmd
-link cmd/tar.snippets                       .config/cmd
-link cmd/zip.snippets                       .config/cmd
