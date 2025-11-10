ed460e25e6b6ab691dc34c2dc3630f576a91f2dc
Chad Skeeters <chad.skeeters@gmail.com>
2025-11-09 18:40:42 -0600

    Updates conceal.nvim configuration

--- nvim/lua/plugins/conceal.lua
+++ nvim/lua/plugins/conceal.lua
@@ -1,19 +1,11 @@
 return {
-    enabled = false,
+    enabled = true,
+    'cskeeters/conceal.nvim',
 
     -- Local plugin under development
-    name="conceal.nvim",
-    dir=os.getenv("HOME").."/working/nvim-plugins/conceal.nvim",
+    -- name="conceal.nvim",
+    -- dir=os.getenv("HOME").."/working/nvim-plugins/conceal.nvim",
 
     lazy = false,
-
-    dependencies = {
-        'libnotify.nvim',
-    },
-
-    config = function()
-        require('conceal').setup({})
-
-        vim.keymap.set({'n'}, '<Leader>z', require('conceal').enable_unicode, { desc="Enable unicode extmarks" })
-    end,
+    opts = {},
 }
