cac3ada4b33721adbc8cbfe614ab16567ace5442
Chad Skeeters <chad.skeeters@gmail.com>
2025-11-21 08:42:59 -0600

    Updates PATH with gemdir/bin

--- shell/bashrc.d-arya/ruby.sh
+++ shell/bashrc.d-arya/ruby.sh
@@ -5,4 +5,8 @@ export LDFLAGS="-L$BREW_PREFIX/opt/ruby/lib"
 export CPPFLAGS="-I$BREW_PREFIX/opt/ruby/include"
 export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$BREW_PREFIX/opt/ruby/lib/pkgconfig"
 
+# GEM_PATH
+export PATH="$(gem env gemdir)/bin:$PATH"
+
+
 #export PATH="$BREW_PREFIX/lib/ruby/gems/3.2.0/bin:$PATH"
