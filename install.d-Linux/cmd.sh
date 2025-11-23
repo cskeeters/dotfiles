451d1b155a1c3452dece5a070a6729544a8287e9
Chad Skeeters <chad.skeeters@gmail.com>
2025-11-23 14:06:53 -0600

    Adds rpmbuild snippets

--- install.d-Linux/cmd.sh
+++ install.d-Linux/cmd.sh
@@ -1,5 +1,6 @@
 if command -v rpm >/dev/null 2>&1 then
     link cmd/rpm.snippets                       .config/cmd
+    link cmd/rpmbuild.snippets                  .config/cmd
 fi
 
 if command -v dnf >/dev/null 2>&1 then
