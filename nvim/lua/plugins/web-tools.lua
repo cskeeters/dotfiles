b449af063c54d262350d942db410bf796eeea352
Chad Skeeters <chad.skeeters@gmail.com>
2025-11-23 23:49:38 -0600

    Adds comments to web-tools

--- nvim/lua/plugins/web-tools.lua
+++ nvim/lua/plugins/web-tools.lua
@@ -1,3 +1,17 @@
+-- BrowserSync {args}	run browser-sync server with args
+-- BrowserOpen {args}	open browser-sync, if browser-sync is not start, start it with args
+-- BrowserPreview {-f --port 3000}	preview current file with browser sync
+-- BrowserRestart	restart browser sync
+-- BrowserStop	stop browser sync
+-- TagRename {newname}	rename html tag
+-- HurlRun {args}	Run Hurl, when in Visual mode, run selected snippets
+-- Npm {args}	Run npm, e.g. npm -i package-name
+-- Yarn {args}	Run yarn, e.g. yarn add package-name
+-- Pnpm {args}	Run pnpm, e.g. pnpm add package-name
+-- Npx {args}	Run npx, e.g. npx add package-name
+-- Node {args}	Run node, e.g. node script.js
+-- JobStop {jobid}	Stop job by jobid, stop last unfinished job if jobid is nil
+
 return {
     enabled = true,
     'ray-x/web-tools.nvim',
