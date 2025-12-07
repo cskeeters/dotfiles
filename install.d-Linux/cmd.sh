bbb1c042886f76a8ce1b1f8ce9b0fbd9cc80f8e0
Chad Skeeters <chad.skeeters@gmail.com>
2025-12-07 09:05:04 -0600

    Fixes formatting of linux cmd snippets installation

--- install.d-Linux/cmd.sh
+++ install.d-Linux/cmd.sh
@@ -1,8 +1,8 @@
 if command -v rpm >/dev/null 2>&1 then
-    link cmd/rpm.snippets                       .config/cmd
-    link cmd/rpmbuild.snippets                  .config/cmd
+    link cmd/rpm.snippets                   .config/cmd
+    link cmd/rpmbuild.snippets              .config/cmd
 fi
 
 if command -v dnf >/dev/null 2>&1 then
-    link cmd/dnf.snippets                       .config/cmd
+    link cmd/dnf.snippets                   .config/cmd
 fi
