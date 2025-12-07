fba332b6a4f9470595a96a2d783cd4517b61a8b1
Chad Skeeters <chad.skeeters@gmail.com>
2025-12-07 16:35:33 -0600

    fixes install script for linux

--- install.d-Linux/cmd.sh
+++ install.d-Linux/cmd.sh
@@ -1,9 +1,9 @@
-if command -v rpm >/dev/null 2>&1 then
+if command -v rpm >/dev/null 2>&1; then
     link cmd/rpm.snippets                   .config/cmd
     link cmd/rpmbuild.snippets              .config/cmd
 fi
 
-if command -v dnf >/dev/null 2>&1 then
+if command -v dnf >/dev/null 2>&1; then
     link cmd/dnf.snippets                   .config/cmd
 fi
 
