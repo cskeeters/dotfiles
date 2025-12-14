c928c20d9501a5430a4e2fe124d15bf70adabb79
Chad Skeeters <chad.skeeters@gmail.com>
2025-12-13 18:18:25 -0600

    nvim: modifies nvim_min to match nvim for typst_compile_open

--- nvim_min/ftplugin/typst.lua
+++ nvim_min/ftplugin/typst.lua
@@ -9,6 +9,33 @@ vim.opt.iskeyword:append('-')
 vim.opt_local.briopt="list:-1"
 vim.opt_local.formatlistpat="^\\s*\\d\\+\\.\\s\\+\\|^\\s*[-*+]\\s\\+\\|^\\[^\\ze[^\\]]\\+\\]:\\&^.\\{4\\}\\|^[>[:space:]]\\+\\s\\+"
 
+vim.opt_local.makeprg = 'typst compile ' .. vim.fn.expand('%')
+vim.opt_local.errorformat='%f:%l:%c:%m,%f:%l:%m'
+
+-- unique augroup name
+local augroup_name = "AutoOpenQuickfixGroup"
+vim.api.nvim_create_augroup(augroup_name, { clear = true })
+
+vim.api.nvim_create_autocmd("QuickFixCmdPost", {
+    pattern = "make", -- Matches ANY command that uses the quickfix list (:make, :grep, etc.)
+    group = augroup_name,
+    nested = true, -- Allows syntax highlighting in the quickfix window
+    callback = function()
+        vim.cmd("cwindow")
+
+        local qflist = vim.fn.getqflist()
+        if #qflist == 0 then
+            local pdf_filename = vim.fn.expand("%:r")..".pdf"
+            local escaped_pdf_filename = vim.fn.shellescape(pdf_filename)
+            local open_cmd = 'open "' .. pdf_filename ..'"'
+            if vim.b.pdf_app ~= nil then
+                open_cmd = 'open -a "'..vim.b.pdf_app..'" '.. escaped_pdf_filename
+            end
+            os.execute(open_cmd)
+        end
+    end,
+})
+
 
 if vim.fn.has("patch-7.4-353") == 0 then
     vim.bo.list = false
@@ -39,91 +66,14 @@ vim.keymap.set('n', '<LocalLeader>a', [[lBi<<Esc>Ea><Esc>]],          { buffer=t
 vim.keymap.set('v', '<LocalLeader>[', [[s[<C-r>"]()<Esc>i]], { buffer=true, desc='Makes selected text a [link](url)' })
 vim.keymap.set('v', '<LocalLeader>l', [[s<<C-r>"><Esc>]],    { buffer=true, desc='Makes selected text a <link>' })
 
--- build
-function typst_compile_open(app)
+local function typst_compile_open(app)
+    -- Return closure with `app` in the context
     return function()
-
-        local uv = vim.uv
-
-        -- This ensures that files can have single quotes in the filenames
-        local src = vim.fn.shellescape(vim.fn.expand("%"))
-        local out = vim.fn.shellescape(vim.fn.expand("%:r"))..".pdf"
-        vim.cmd.update()
-
-        -- Run and show error message
-        -- local open = 'open ' .. out
-        -- if app ~= nil then
-        --     open = 'open -a '..app..' '..out
-        -- end
-        -- local cmd = 'typst compile '..src..' '..out..' && '..open
-        -- vim.cmd("!"..cmd)
-
-        -- execute, no error message
-        -- local err = os.execute(cmd)
-        -- if err == 0 then
-            -- if app == nil then
-                -- os.execute('open '..out)
-            -- else
-                -- os.execute('open -a '..app..' '..out)
-            -- end
-        -- else
-            -- vim.notify("typst_compile_open: Error executing: "..cmd, vim.log.levels.ERROR)
-        -- end
-
-
-        local stdout = uv.new_pipe()
-        local stderr = uv.new_pipe()
-
-        -- local cmd = 'typst compile '..src..' '..out
-        local cmd = 'echo "hi"'
-        local err = {}
-
-        local handle, pid = uv.spawn('typst', {
-            args = {
-                'compile',
-                vim.fn.expand("%"),
-                vim.fn.expand("%:r")..".pdf",
-            },
-            stdio = {nil, stdout, stderr}
-        }, function(code, signal) -- on exit
-            if code == 0 then
-                vim.notify("typst_compile_open: Successfully built: "..out, vim.log.levels.INFO)
-                local open = 'open ' .. out
-                if app ~= nil then
-                    open = 'open -a "'..app..'" '..out
-                end
-                os.execute(open)
-            else
-                vim.notify("typst_compile_open: "..out, vim.log.levels.INFO)
-
-                uv.read_start(stderr, function(err, data)
-                    if err then
-                        vim.notify("typst_compile_open: Error reading stderr", vim.log.levels.ERROR)
-                    else
-                        if data then
-                            vim.notify("typst_compile_open: Error building: "..out.."\n"..data, vim.log.levels.ERROR)
-                        end
-                    end
-                end)
-
-            end
-
-            -- print("exit code", code)
-            -- print("exit signal", signal)
-        end)
-
-        vim.notify("typst_compile_open: Compiling: "..vim.fn.expand("%"), vim.log.levels.INFO)
-
-        -- uv.shutdown(stdin, function()
-            -- print("stdin shutdown", stdin)
-            -- uv.close(handle, function()
-                -- print("process closed", handle, pid)
-            -- end)
-        -- end)
-
-
+        vim.cmd.update() -- Save buffer
+        vim.b.pdf_app = app -- Set default app
+        vim.cmd([[:silent make]]) -- `silent` so user doesn't have to press enter after
     end
 end
 
-vim.keymap.set('n', '<C-k>d', typst_compile_open("Adobe Acrobat.app"), { buffer=true, desc='Build/Compile to PDF, open in Default app (Acrobat)' })
-vim.keymap.set('n', '<C-k>p', typst_compile_open("Preview.app"), { buffer=true, desc='Build/Compile to PDF, open in Preview.app' })
+vim.keymap.set('n', '<C-k>d', typst_compile_open("Adobe Acrobat.app"), { buffer=true, desc='Build/Compile to PDF, open in Acrobat.app' })
+vim.keymap.set('n', '<C-k>p', typst_compile_open("Preview.app"),       { buffer=true, desc='Build/Compile to PDF, open in Preview.app' })
