5774b055003dc588291fa794779c903df3574f03
Chad Skeeters <chad.skeeters@gmail.com>
2025-12-27 15:00:04 -0600

    restic: Configuration for arya

---
+++ install.d-arya/restic.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# net.restic.plist calls restic_backup, which is located in ~/bin
+link restic-arya/net.restic.plist      Library/LaunchAgents
+link restic-arya/restic                .config
