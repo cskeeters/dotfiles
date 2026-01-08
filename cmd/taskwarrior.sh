6501fc3de9437962d89c4943f25183b1f16c33af
Chad Skeeters <chad.skeeters@gmail.com>
2026-01-07 18:00:50 -0600

    cmdsnip: taskwarrior: colored task selection

--- cmd/taskwarrior.sh
+++ cmd/taskwarrior.sh
@@ -2,9 +2,18 @@ tw_task() {
     PROMPT="$1"
     shift
 
+    # jq will output
+    # * numerical date for sort -n
+    # * uuid for selection
+    # * string with ansi color coding to search on with fzf
     task $* export | \
-        jq -r '.[] | "\(.uuid)\t\(.description)"' | \
-        fzf -d $'\t' --with-nth 2.. --accept-nth 1 --prompt "$PROMPT> "
+        jq -r '.[] |
+            (if .tags == null then "" else .tags | map("+"+.) | join(" ") end) as $tags |
+            ( .entry |
+              sub("(?<y>\\d{4})(?<m>\\d{2})(?<d>\\d{2})T(?<H>\\d{2})(?<M>\\d{2})(?<s>\\d{2})Z"; "\(.y)-\(.m)-\(.d)T\(.H):\(.M):\(.s)Z") |
+              fromdateiso8601) as $datestr |
+            "\($datestr)\t\(.uuid)\t\u001b[34m\($datestr | strflocaltime("%Y-%m-%d")) \u001b[0m\(.description) \u001b[32m\($tags)\u001b[0m"' | \
+            sort -k1 -nr | fzf --ansi -d $'\t' --with-nth 3.. --accept-nth 2 --prompt "$PROMPT> "
 }
 
 tw_date() {
