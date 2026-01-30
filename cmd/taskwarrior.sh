7a64f76514d380cd4d12629c09e8186e7fb4cb32
Chad Skeeters <chad.skeeters@gmail.com>
2026-01-30 15:22:50 -0600

    cmdsnip: task: Mod Due Date only shows task with due date

--- cmd/taskwarrior.sh
+++ cmd/taskwarrior.sh
@@ -13,7 +13,26 @@ tw_task() {
               sub("(?<y>\\d{4})(?<m>\\d{2})(?<d>\\d{2})T(?<H>\\d{2})(?<M>\\d{2})(?<s>\\d{2})Z"; "\(.y)-\(.m)-\(.d)T\(.H):\(.M):\(.s)Z") |
               fromdateiso8601) as $datestr |
             "\($datestr)\t\(.uuid)\t\u001b[34m\($datestr | strflocaltime("%Y-%m-%d")) \u001b[0m\(.description) \u001b[32m\($tags)\u001b[0m"' | \
-            sort -k1 -nr | fzf --ansi -d $'\t' --with-nth 3.. --accept-nth 2 --prompt "$PROMPT> "
+            sort -k3 -n | fzf --ansi -d $'\t' --with-nth 3.. --accept-nth 2 --prompt "$PROMPT> "
+}
+
+tw_task_due() {
+    PROMPT="$1"
+    shift
+
+    # jq will output
+    # * numerical date for sort -n
+    # * uuid for selection
+    # * string with ansi color coding to search on with fzf
+    task $* export | \
+        jq -r '.[] |
+            select(.due) |
+            (if .tags == null then "" else .tags | map("+"+.) | join(" ") end) as $tags |
+            ( .due |
+              sub("(?<y>\\d{4})(?<m>\\d{2})(?<d>\\d{2})T(?<H>\\d{2})(?<M>\\d{2})(?<s>\\d{2})Z"; "\(.y)-\(.m)-\(.d)T\(.H):\(.M):\(.s)Z") |
+              fromdateiso8601) as $datestr |
+            "\($datestr)\t\(.uuid)\t\u001b[34m\($datestr | strflocaltime("%Y-%m-%d")) \u001b[0m\(.description) \u001b[32m\($tags)\u001b[0m"' | \
+            sort -k3 -n | fzf --ansi -d $'\t' --with-nth 3.. --accept-nth 2 --prompt "$PROMPT> "
 }
 
 tw_date() {
