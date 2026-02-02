f226b185b3002989770a02451a772897e00fda28
Chad Skeeters <chad.skeeters@gmail.com>
2026-02-01 21:43:33 -0600

    cmdsnip: ollama: Adds ps and stop

--- cmd/ollama.sh
+++ cmd/ollama.sh
@@ -1,3 +1,7 @@
 ollama_local_models() {
     ollama list | grep -v "NAME" | cut -d " " -f 1 | fzf --prompt "MODEL> "
 }
+
+ollama_running_models() {
+    ollama ps | sed '1d' | awk '{print $1}' | fzf --prompt "RUNNING MODEL> "
+}
