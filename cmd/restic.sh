6ee5224df91c677cd84a07e4efbcd4b85f0a506f
Chad Skeeters <chad.skeeters@gmail.com>
2026-02-06 10:06:14 -0600

    Moves restic respository to cmdsnip.  Requires bin/restic_repo_activate!

--- cmd/restic.sh
+++ cmd/restic.sh
@@ -1,3 +1,7 @@
+restic_repo() {
+    cat ~/.restic/repos | fzf --with-nth 1 --accept-nth 1 --prompt "REPO> "
+}
+
 restic_backedup_path() {
     restic snapshots --latest 1 --host $(hostname) --json | jq -r '.[] | .paths[] ' | uniq | sort | \
         FZF_DEFAULT_OPTS="$FZF_NO_PREVIEW_OPTS" fzf --prompt "PATH> "
