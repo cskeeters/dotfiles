387b3082b42278bd3059d4b95f486e05fe990282
Chad Skeeters <chad.skeeters@gmail.com>
2026-02-19 18:22:20 -0600

    nvim: qalc

---
+++ nvim/lua/plugins/qalc.lua
@@ -0,0 +1,11 @@
+return {
+    enabled = true,
+    'Apeiros-46B/qalc.nvim',
+    lazy = false,
+    -- ft = "qalc",
+    init = function()
+    end,
+    config = function()
+        -- vim.keymap.set('n', '<leader>S', [[<Cmd>Soulver<Cr>]], { desc="Run Soulver" });
+    end
+}
