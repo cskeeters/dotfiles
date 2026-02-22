a7b152843f177c8479d6f1d1efcdb1fbdcfc3bfb
Chad Skeeters <chad.skeeters@gmail.com>
2026-02-22 12:58:48 -0600

    cmdsnip: diskutil: webdav mount/umount

--- cmd/diskutil.sh
+++ cmd/diskutil.sh
@@ -5,3 +5,13 @@ diskutil_disknum_by_parition_name() {
         sed -nre "s~disk(.)([^[:space:]]+) (.*)~\1 \3~p" | \
         FZF_DEFAULT_OPTS="$FZF_NO_PREVIEW_OPTS" fzf --with-nth 2.. --accept-nth 1
 }
+
+diskutil_webdav_volume() {
+    cat ~/.webdav | \
+        FZF_DEFAULT_OPTS="$FZF_NO_PREVIEW_OPTS" fzf -1 --prompt="VOLUME>"
+}
+
+diskutil_mounted_webdav_volume() {
+    mount | sed -nre 's;.* (/Volumes/[^[:space:]]+) .*webdav.*;\1;p' | \
+        fzf -1 --prompt="VOLUME>"
+}
