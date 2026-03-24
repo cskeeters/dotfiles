18e62bccb3dc873d66a5eb5b7d913dac844b4801
Chad Skeeters <chad.skeeters@gmail.com>
2026-03-23 21:22:35 -0400

    nvim: ctags bin path

--- nvim/lua/plugins/tagbar.lua
+++ nvim/lua/plugins/tagbar.lua
@@ -5,6 +5,7 @@ return {
     init = function()
         vim.g.tagbar_autofocus=1
         vim.g.tagbar_autoclose=1
+        vim.g.tagbar_ctags_bin = '/usr/local/bin/ctags'
     end,
     keys = {
         {'<Leader>T', ':TagbarToggle<cr>', noremap=true, silent=true, desc="Toggles the Tagbar" },
