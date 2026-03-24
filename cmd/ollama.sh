c549fb1d856294840ca9ed04a985d2c2f51238b5
Chad Skeeters <chad.skeeters@gmail.com>
2026-03-24 14:28:44 -0500

    cmdsnip: updates ollama snippets

--- cmd/ollama.sh
+++ cmd/ollama.sh
@@ -1,5 +1,30 @@
 ollama_local_models() {
-    ollama list | grep -v "NAME" | cut -d " " -f 1 | fzf --prompt "MODEL> "
+    OLLAMA_LIST=$(ollama list)
+    if [[ $? -ne 0 ]]; then
+        cmd_error "Error running ollama list.  Is the ollama server running?"
+    else
+        echo "$OLLAMA_LIST" | grep -v "NAME" | cut -d " " -f 1 | \
+            FZF_DEFAULT_OPTS="$FZF_NO_PREVIEW_OPTS" fzf --prompt "MODEL> "
+    fi
+}
+
+ollama_local_manifest() {
+    cmd_debug "Running ollama_local_manifest"
+    ollama list | grep -v "NAME" | cut -d " " -f 1 | tr ":" "/" | fzf --prompt "MODEL> "
+}
+
+ollama_model_blob_path() {
+    MODEL="$(ollama_local_models)"
+    MANIFEST_SUBPATH="$(echo "$MODEL" | tr ":" "/")"
+    MANIFEST_PATH="~/.ollama/models/manifests/registry.ollama.ai/library/$MANIFEST_SUBPATH"
+    cmd_debug "Manifest for $MODEL: $MANIFEST_PATH"
+
+    BLOB_FILE=$(cat ~/.ollama/models/manifests/registry.ollama.ai/library/$MANIFEST_SUBPATH | \
+        jq -r '.layers[] | select (.mediaType == "application/vnd.ollama.image.model") | .digest' | \
+        tr ':' '-')
+    BLOB_PATH="~/.ollama/models/blobs/$BLOB_FILE"
+    cmd_info "BLOB for $MODEL: $BLOB_PATH"
+    echo $BLOB_PATH
 }
 
 ollama_running_models() {
